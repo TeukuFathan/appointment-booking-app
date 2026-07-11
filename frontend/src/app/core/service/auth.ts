ba4b479659460cc836f11ee8f2a200b73976c8ac
TeukuFathan <teukufathan.f@gmail.com>
2026-07-11 14:04:05 +0200

    auth.ts added authentification system for the backend

--- frontend/src/app/core/service/auth.ts
+++ frontend/src/app/core/service/auth.ts
@@ -1,6 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+
+  private http = inject(HttpClient);
+
+
+  login(data: { email: string; password: string }) {
+    return this.http
+      .post<{ token: string }>(
+        `${environment.apiUrl}/auth/login`,
+        data
+      )
+      .pipe(
+        tap(response => {
+          localStorage.setItem('token', response.token);
+        })
+      );
+  }
+
+  register(data: {
+    name: string;
+    email: string;
+    password: string;
+    businessName: string;
+    businessSlug: string;
+  }) {
+    return this.http.post(
+      `${environment.apiUrl}/auth/register`,
+      data
+    );
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
+
